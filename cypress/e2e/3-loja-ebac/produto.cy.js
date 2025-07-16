6296676bfddefad49c335a249974db96bb3651a8
Dev-DaMata <gcordeiro773@gmail.com>
2025-07-16 11:34:48 -0300

    criando cenarios de teste do produto

--- cypress/e2e/3-loja-ebac/produto.cy.js
+++ cypress/e2e/3-loja-ebac/produto.cy.js
@@ -6,13 +6,33 @@ describe('Funcionalidade: Produtos', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
 
-    
-    it('Deve selecionar um produto da lista', () =>{
+    it('Deve selecionar um produto da lista', () => {
         cy.get('.products > .row')
-            //.first()//para capturar o primeiro elemento da lista
-            //.last()//para capturar o ultimo elemento da lista
-            //.eq(2)//para capturar um especifico na lista pela posição dele 
             .contains('Apollo Running Short')//para capturar pelo nome do produto na lista
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        cy.get('.single_add_to_cart_button').should('exist')
+    })
+
+    it('Deve selecionar o primeiro produto da lista', () => {
+        cy.get('.product-block')
+            .first()//para capturar o primeiro elemento da lista
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        cy.get('.single_add_to_cart_button').should('exist')
+    })
+
+    it('Deve selecionar o ultimo produto da lista', () => {
+        cy.get('.product-block')
+            .last()//para capturar o ultimo elemento da lista
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        cy.get('.single_add_to_cart_button').should('exist')
+    })
+
+    it('Deve selecionar o 3° produto da lista', () => {
+        cy.get('.product-block')
+            .eq(2)//para capturar um especifico na lista pela posição dele 
             .click()
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
         cy.get('.single_add_to_cart_button').should('exist')
